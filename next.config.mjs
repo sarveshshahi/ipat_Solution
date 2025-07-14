b2af9fe14b378836dc899e9af612ee610d241df1
Sarvesh Shahi <sarveshshahi1210@gmail.com>
2025-07-14 20:22:18 +0530

    Update next.config.js and package.json for improved build configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  devIndicators: false
+  devIndicators: false,
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
